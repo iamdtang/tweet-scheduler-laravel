ec2ac5b5a6c4cd1f715b862663956038ff4b577a
David Tang <dtang859@gmail.com>
2018-04-29 23:05:13 -0700

    post tweet endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 Route::middleware(['twitter'])->group(function () {
     Route::get('/', function () {
         return view('home');
@@ -17,15 +19,29 @@ Route::get('/login/twitter', function () {
 Route::get('/login/twitter/callback', function () {
     $user = Socialite::driver('twitter')->user();
     session([
-      'twitter-user' => $user
+        'twitter-user' => $user
     ]);
     return redirect('/');
 });
 
 Route::get('/logout', function () {
     session([
-      'twitter-user' => null
+        'twitter-user' => null
     ]);
 
     return redirect('/login');
+});
+
+Route::post('/tweets', function () {
+    $user = session('twitter-user');
+    $connection = new TwitterOAuth(
+        env('TWITTER_CONSUMER_KEY'),
+        env('TWITTER_CONSUMER_SECRET'),
+        $user->token,
+        $user->tokenSecret
+    );
+
+    $connection->post('statuses/update', [
+        'status' => $request->body
+    ]);
 });
